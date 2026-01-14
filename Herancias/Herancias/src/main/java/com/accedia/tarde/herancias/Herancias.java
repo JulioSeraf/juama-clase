49382091ae1f0a281482597ff48eca86ceaddbdd
julio <juliodev26@gmail.com>
2026-01-14 20:19:40 +0100

    adicional a herancia

--- Herancias/Herancias/src/main/java/com/accedia/tarde/herancias/Herancias.java
+++ Herancias/Herancias/src/main/java/com/accedia/tarde/herancias/Herancias.java
@@ -78,5 +78,6 @@ public class Herancias {
         System.out.println("=============================== Ejercicio 15 ==============================");
         Animal a = new Perro();
         a.hacerSonido();
+        System.out.println("El tipo de la referencia es Animal, el tipo real Perro, se ejecuta el metodo mas cercano al tipo real o sea el de Perro");
     }
 }
