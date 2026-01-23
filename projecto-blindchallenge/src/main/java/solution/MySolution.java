3f5a101076fd1fcbbb92133536b18f7b63a838ab
julio serafim <juliodev26@gmail.com>
2026-01-23 13:11:45 +0100

    readicionado blindChallenge

---
+++ projecto-blindchallenge/src/main/java/solution/MySolution.java
@@ -0,0 +1,13 @@
+package solution;
+
+import blch.framework.annotations.Solution;
+import blch.framework.external.SolutionBase;
+
+@Solution
+public class MySolution extends SolutionBase {
+
+  @Override
+  public Object execute(Object... args) {
+    return null;
+  }
+}
