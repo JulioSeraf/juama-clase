5e58c345337e15f1d3293f0a30903b77b762e7c4
julio serafim <juliodev26@gmail.com>
2026-01-24 12:49:05 +0100

    blinchallenge TERMINADO.

--- projecto-blindchallenge/src/main/java/solution/MySolution.java
+++ projecto-blindchallenge/src/main/java/solution/MySolution.java
@@ -3,23 +3,27 @@ package solution;
 import blch.framework.annotations.Solution;
 import blch.framework.external.SolutionBase;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @Solution
 public class MySolution extends SolutionBase {
 
-  public static HashMap<Integer, Person> info = new HashMap<>();
-  public int count = 0;
+  public Map<Integer, Person> map = new HashMap<>();
+  public int idPer = 1;
 
   public Person createPerson(Object... args) {
     Person per = new Person();
     per.setName(args[2].toString());
-    if (args.length > 3) {
+    if (args.length > 3 && args[3].equals("age")) {
       if (args[4].getClass().getSimpleName().equals("Integer")) {
         int age = (int) args[4];
         if (age <= 0) {
           throw new IllegalArgumentException("Invalid age");
         }
         per.setAge(age);
+      } else {
+        throw new IllegalArgumentException("Invalid age");
       }
     }
 
@@ -38,32 +42,40 @@ public class MySolution extends SolutionBase {
     if (args.length == 1) {
       throw new IllegalArgumentException("Missing entity");
     }
-    if (args.length == 2) {
-      return 0;
+    if (args[0].equals("COUNT") || args[0].equals("SIZE")) {
+      return map.size();
     }
-    System.out.println(info.size());
 
-    System.err.println(person.size());
-
+    if (!args[1].equals("person")) {
+      throw new IllegalArgumentException("Unknown entity");
+    }
+    if (!args[0].equals("PUT") && !args[0].equals("GET") && !args[0].equals("REMOVE")) {
+      throw new IllegalArgumentException("Unknown command");
+    }
+    System.out.println(map.size());
     if (args[0].equals("PUT")) {
-      if (info.isEmpty()) {
-        info.put(count, createPerson(args));
+      if (args.length == 2) {
+        throw new IllegalArgumentException("Missing name");
+      }
+      if (args[2].equals("")) {
+        throw new IllegalArgumentException("Invalid name");
+      }
+      if (map.isEmpty()) {
+        map.put(idPer, createPerson(args));
       } else {
-        info.forEach(
+        map.forEach(
             (k, p) -> {
               if (p.getName().equals(args[2])) {
                 throw new IllegalArgumentException("Already exists");
               }
             });
-        count++;
-        info.put(count, createPerson(args));
+        idPer++;
+        map.put(idPer, createPerson(args));
       }
-
       return true;
     }
-
     if (args[0].equals("GET")) {
-      info.forEach(
+      map.forEach(
           (k, p) -> {
             if (p.getName().equals(args[2])) {
               person.put("name", p.getName());
@@ -77,23 +89,21 @@ public class MySolution extends SolutionBase {
           });
       if (person.isEmpty()) {
         throw new IllegalArgumentException("Not found");
-      } else {
-        return person;
       }
+      return person;
     }
 
     if (args[0].equals("REMOVE")) {
-      info.forEach(
-          (k, p) -> {
-            if (p.getName().equals(args[2])) {}
-          });
-      count--;
-      return true;
+      Iterator<Map.Entry<Integer, Person>> info = map.entrySet().iterator();
+      while (info.hasNext()) {
+        Map.Entry<Integer, Person> objPer = info.next();
+        if (objPer.getValue().getName().equals(args[2])) {
+          info.remove();
+          return true;
+        }
+      }
+      throw new IllegalArgumentException("Not found");
     }
-    if (count >= 1) {
-      return count;
-    }
-
-    return false;
+    throw new NullPointerException();
   }
 }
